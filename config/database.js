497e7a61314a02640a7a380ed36dbd54f33a996c
irfan shah <programmer.irfansha@hotmail.com>
2023-12-16 00:03:47 +0500

    miner chagnes

--- config/database.js
+++ config/database.js
@@ -1,13 +1,13 @@
 import mongoose from "mongoose";
-import colors from "colors";
-const connectDb = async ()=>{
-    try {
-        const connection = await mongoose.connect(process.env.CONNCETIONSTRING);
-        console.log(`connected to MongosDb ${connection.connection.name}`.bgMagenta.white)
-    } catch (error) {
-        console.log("error while connecting to Db".bgRed.white)
-    }
-}
-
+const connectDb = async () => {
+  try {
+    const connection = await mongoose.connect(process.env.CONNCETIONSTRING);
+    console.log(
+      `connected to MongosDb ${connection.connection.name}`.bgMagenta.white
+    );
+  } catch (error) {
+    console.log("error while connecting to Db".bgRed.white);
+  }
+};
 
 export default connectDb;
